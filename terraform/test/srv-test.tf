aa4bc606e09cb6578b844097a1db5c9784993c7d
Linus Baumann <keen.key5715@linus-baumann.de>
2025-02-01 17:31:51 +0100

    second try

--- terraform/test/srv-test.tf
+++ terraform/test/srv-test.tf
@@ -1,10 +1,15 @@
 resource "proxmox_vm_qemu" "srv-test" {
     name = "srv-test"
-    desc = "TF Test"
-    vmid = "100248"
     target_node = "pve-s13-02"
-
-    clone = "ubuntu-cloud-init-2"
+    vmid = "100248"
+    desc = "TF Test"
+    bios = "ovmf"
+    onboot = true
+    vm_state = "running"
+    agent = 1
+    clone_id = "7002"
+    full_clone = true
+    scsihw = "virtio-scsi-pci"
     cores = 2
     sockets = 1
     cpu_type = "host"
@@ -12,6 +17,14 @@ resource "proxmox_vm_qemu" "srv-test" {
 
     os_type = "cloud-init"
     ipconfig0 = "ip=10.0.100.248/24,gw=10.0.100.1"
+    ciuser = "mgrsys"
+    ciupgrade = true
+    searchdomain = "local.darksaber.fyi"
+    nameserver = "10.0.100.1"
+    sshkeys = <<EOF
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK43atgPI2XVBwvSM5hGhbeokxgKuetThnThHEJRMiAK
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuOhwlgxLF/4v06qha9jPQ4egOaW2nUFlC7988tPh9X
+    EOF
 
     network {
         id = 0
